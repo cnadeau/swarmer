d567d390d8259baef11f97834a0c08c9b6300f6e
Christian Nadeau <cnadeau@macadamian.com>
2016-04-13 22:31:25 -0400

    Stop and remove swarm-agent to remove the master from the swarm nodes

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,14 @@ docker-machine create \
   --engine-opt="cluster-advertise=eth1:2376" \
   swarm-master
 
+# Stop and remove swarm-agent to remove the master from the swarm nodes (https://github.com/docker/machine/issues/2302)
+eval $(docker-machine env swarm-master)
+docker stop swarm-agent
+docker rm swarm-agent
+
+# Remove the swarm-master from the docker/swarm/nodes kv
+curl -X "DELETE" http://$(docker-machine ip swarm-keystore):8500/v1/kv/docker/swarm/nodes/$(docker-machine ip swarm-master):2376
+
 docker-machine create \
   -d virtualbox \
   --swarm \
